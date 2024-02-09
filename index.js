a7ee3de84dd06e85680611245f2f8858b80b0256
Krishna Vishwakarma <krishnav@geekyants.com>
2024-02-10 03:15:42 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,8 +4,42 @@ const path = require("path");
 const cors = require("cors");
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+const user = [
+  {
+    id: "raj1",
+    name: "Rajesh De",
+    email: "rajesh@test.com",
+  },
+  {
+    id: "ayush1",
+    name: "Ayush",
+    email: "ayush@test.com",
+  },
+  {
+    id: "nish1",
+    name: "Nishant",
+    email: "nishat@test.com",
+  },
+  {
+    id: "arpit1",
+    name: "Arpit",
+    email: "arpit@test.com",
+  },
+  {
+    id: "atul1",
+    name: "Atul",
+    email: "atul@test.com",
+  },
+  {
+    key: "abc012xyz4",
+    name: "Krishna",
+    email: "krishna@test.com",
+  },
+];
 
 app.use(cors());
 app.use(express.json());
@@ -28,26 +62,61 @@ app.get("/", (req, res) => {
   // });
   res.send("welcome!");
 });
-app.post("/getUser", (req, res, next) => {
+
+function genIat() {
+  return Math.floor(Date.now() / 1000);
+}
+
+app.post("/verifyToken", (req, res, next) => {
   const token = req.body.user_token;
-  // const verify = jwt.verify(token, SECRET);
-  console.log(token);
-  // res.json({
-  //   ...verify,
-  // });
+  const verify = jwt.verify(token, SECRET);
+  res.json({
+    ...verify,
+  });
   res.send({
     jwt: token,
   });
-
   next();
+});
+
+app.post("/getUser", (req, res, next) => {
+  const receivedID = req.body.user_token;
+  const data = user.filter((item) => item.id === receivedID);
+
+  const newJTI = uuidv4();
+  const newIAT = genIat();
+
+  const payload = {
+    name: data[0].name,
+    email: data[0].email,
+    jti: newJTI,
+    iat: newIAT,
+  };
+  const tokenOptions = {
+    expiresIn: "300s",
+    header: { kid: KEY_ID },
+  };
+  jwt.sign(payload, SECRET, tokenOptions, (error, token) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: "Failed to generate token" });
+    } else {
+      res.json({
+        jwt: token,
+      });
+    }
+  });
+
+  // next();
 });
 app.post("/login", (req, res) => {
   const payload = {
     name: req.body.name,
     email: req.body.email,
+    iat: req.body.iat,
+    jti: req.body.jti,
   };
   const tokenOptions = {
-    expiresIn: "300s",
     header: { kid: KEY_ID },
   };
   jwt.sign(payload, SECRET, tokenOptions, (error, token) => {
