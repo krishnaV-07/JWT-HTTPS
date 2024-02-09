2eb7a36643fab3d3065dd72ed187bb9ec661fe0a
Krishna Vishwakarma <krishnav@geekyants.com>
2024-02-09 17:59:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const https = require("https");
+const path = require("path");
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "cert", "key.pem")),
+    cert: fs.readFileSync(path.join(__dirname, "cert", "cert.pem")),
+  },
+  app
+);
+const KEY_ID = "AnMWG3wu6M1J3hPYYqSaZZSM33kPpeLg6VxEK2PM9bWXO8lF";
+const SECRET = "AnMWG3wu6M1J3hPYYqSaZZSM33kPpeLg6VxEK2PM9bWXO8lF";
+app.get("/", (req, res) => {
+  res.json({
+    message: "message from ssl",
+  });
+});
+app.post("/login", (req, res) => {
+  const payload = {
+    jti: 12345,
+    iat: 1639608035,
+    name: "krishna v",
+    email: "krishnv@geekyants.com",
+  };
+  const tokenOptions = {
+    expiresIn: "300s",
+    header: { kid: KEY_ID },
+  };
+  jwt.sign(payload, SECRET, tokenOptions, (error, token) => {
+    if (error) {
+      console.log(error);
+      res.status(500).json({ error: "Failed to generate token" });
+    } else {
+      res.json({
+        token: token,
+      });
+    }
+  });
+});
+
+sslServer.listen(3434, () => {
+  console.log(
+    "app is running at secure server on port  https://localhost:3434/"
+  );
+});
