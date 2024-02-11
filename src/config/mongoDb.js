742b3789aa787a68598253b82b22011d218979e0
Krishna Vishwakarma <krishnav@geekyants.com>
2024-02-11 19:41:17 +0530

    mongoose added

--- src/config/mongoDb.js
+++ src/config/mongoDb.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const DBconnection = async () => {
   try {
-    console.log(process.env.MONGO_URI);
     const conn = await mongoose.connect(process.env.MONGO_URI, {});
     console.log("Connected to the DATABASE");
   } catch (error) {
